3852f451f586789533c896648f0a6b9776b4a205
Douglas Feliphe <douglas.feliphe@going2.com.br>
2022-11-17 15:53:50 -0300

    fix

---
+++ Assets/rotateEarth.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.Events;
+using UnityEngine;
+using Vuforia;
+
+public class RotateEarth : MonoBehaviour
+{
+    public float speed = 10f;
+
+    VirtualButtonBehaviour[] virtualButtonBehaviours;
+
+    public GameObject cube;
+
+
+    void Start()
+    {
+        virtualButtonBehaviours = GetComponentsInParent<VirtualButtonBehaviour>();
+
+        for (int i = 0; i < virtualButtonBehaviours.Length; i++)
+        {
+            virtualButtonBehaviours[i].GetComponentsInParent<VirtualButtonBehaviour>();
+        }
+    }
+
+    //void Update()
+    //{
+    //    transform.Rotate(Vector3.up, speed * Time.deltaTime, Space.World);
+    // }
+}
---
+++ Assets/vbInteractions.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class VbInteractions : MonoBehaviour
+{
+
+    VirtualButtonBehaviour[] virtualButtonBehaviours;
+
+    string vbName;
+
+    public GameObject low_poly_earth, SecondPlanet, cube, cylinder;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        virtualButtonBehaviours = GetComponentsInParent<VirtualButtonBehaviour>();
+
+        for (int i=0; i < virtualButtonBehaviours.Length;i++)
+        {
+            virtualButtonBehaviours[i].RegisterEventHandler(this);
+        }
+    }
+
+    public void onButtonPressed(VirtualButtonBehaviour vb)
+    {
+        vbName = vb.VirtualButtonName;
+
+        if (vbName == "VirtualButton1")
+        {
+            low_poly_earth.SetActive(true);
+        }
+
+        if (vbName == "VirtualButton2")
+        {
+            low_poly_earth.SetActive(false);
+        }
+    }
+
+}
