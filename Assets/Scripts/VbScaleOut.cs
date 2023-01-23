63399f1ac9ad36198e3eed7e271b266778d3b179
Douglas Feliphe <douglas.feliphe@going2.com.br>
2023-01-23 05:51:15 -0300

    virtual button changing to red when on press

--- Assets/Scripts/VbScaleOut.cs
+++ Assets/Scripts/VbScaleOut.cs
@@ -7,6 +7,7 @@ public class VbScaleOut : MonoBehaviour, IVirtualButtonEventHandler
 {
     public GameObject planet;
     public VirtualButtonBehaviour Vb;
+    private Color vbDefaultColor;
     public float planetScale = 0.001f;
     private bool _ZoomOut = false;
 
@@ -29,12 +30,21 @@ public class VbScaleOut : MonoBehaviour, IVirtualButtonEventHandler
     public void OnButtonPressed(VirtualButtonBehaviour vb)
     {
         Debug.Log("VbScaleOut OnButtonPressed");
+
+        // store the default color
+        vbDefaultColor = vb.transform.GetChild(0).GetComponent<Renderer>().material.color;
+        // change color of the button on press
+        vb.transform.GetChild(0).GetComponent<Renderer>().material.color = Color.red;
+
         _ZoomOut = true;
     }
 
     public void OnButtonReleased(VirtualButtonBehaviour vb)
     {
         Debug.Log("VbScaleOut OnButtonReleased");
+
+        vb.transform.GetChild(0).GetComponent<Renderer>().material.color = vbDefaultColor;
+
         _ZoomOut = false;
     }
 }
--- Assets/VbShowHideText.cs
+++ Assets/VbShowHideText.cs
@@ -7,6 +7,8 @@ public class VbShowHideText : MonoBehaviour, IVirtualButtonEventHandler
 {
     public GameObject text;
     public VirtualButtonBehaviour Vb;
+    private Color vbDefaultColor;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -15,13 +17,23 @@ public class VbShowHideText : MonoBehaviour, IVirtualButtonEventHandler
 
     public void OnButtonPressed(VirtualButtonBehaviour vb)
     {
-        Debug.Log("VbShowHideText OnButtonPressed");
+        Debug.Log("VbShowHideText OnButtonPressed");  
+        
+        // store the default color
+        vbDefaultColor = vb.transform.GetChild(0).GetComponent<Renderer>().material.color;
+        // change color of the button on press
+        vb.transform.GetChild(0).GetComponent<Renderer>().material.color = Color.red;
+
+
         text.SetActive(true);
     }
 
     public void OnButtonReleased(VirtualButtonBehaviour vb)
     {
         Debug.Log("VbShowHideText OnButtonReleased");
+
+        vb.transform.GetChild(0).GetComponent<Renderer>().material.color = vbDefaultColor;
+
         text.SetActive(false);
     }
 }
